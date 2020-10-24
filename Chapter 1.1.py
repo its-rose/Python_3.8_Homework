3497b72f901da58bec5e0d0941bf8f17e5199aac
rosepython <seitakhunova@gmail.com>
2020-10-24 12:20:27 +0600

    Homework

---
+++ Chapter 1.1.py
@@ -0,0 +1,71 @@
+# Задание 1. Код для ввода двух чисел
+# number1 = int(input('Введите первое число   '))
+# number2 = int(input('Введите второе число   '))
+# Задание 1. Вычислите разницу чисел
+# print(number1 - number2)
+
+# Задание 2. Вычислите остаток деления 36 на 5
+# number3 = 36
+# number4 = 5
+# print(number3 / number4)
+# print(number3 % number4)
+
+# Задание 3. Проверка слова, является ли оно палиндромом
+# text = input('Введите текст ')
+# if text == text[::-1]:
+#     print('Палиндром')
+# else:
+#     print('Не палиндром')
+
+# Задание 4. I love Java. Вывести 1 и 2 слово, добавив I love Java / Python
+# string = 'I love Java'
+# print(string[0:6:])
+# print(string[0:6:] + ' ' + string)
+# string2 = 'Python'
+# print(string[0:6:] + ' ' + string2)
+# print(string.replace('Java', 'Python'))
+
+
+# Задание 5. Код для ввода текста. Текст выведенный 10 раз.
+# text = input('Введите Ваше имя ')
+# q = 10
+# print(text * q)
+
+# Задание 6. Ввод текста и вывод букв в обратном порядке.
+# text = input('Введите текст ')
+# print(text [::-1])
+
+# Задание 7. Ввод числа. Вывести его 10 цифру по порядку.
+# dig = input('Введите число ')
+# print(dig[10:11:])
+
+# Задание 8. Код для получения первых и последних 2 символов в строке.
+# string = input('Введите текст ')
+# print(string[0] + string[1] + string[-2] + string[-1])
+# if len(string) == 2:
+#     print(string * 2)
+# if len(string) <=2:
+#     print('Error')
+
+# Задание 9. Код для ввода числа и вывода предыдущего и следующего от него числа.
+# dig = int(input('Введите число '))
+# print(dig)
+# print(dig - 1)
+# print(dig + 1)
+
+# Задание 10. Ввод двух чисел, сравнение их, вывод результата.
+# dig1 = input('Введите первое число ')
+# dig2 = input('Введите второе число ')
+# if dig1 > dig2:
+#     print(dig1 + '>' + dig2)
+# if dig1 < dig2:
+#     print(dig1 + '<' + dig2)
+# if dig1 == dig2:
+#     print(dig1 + '=' + dig2)
+
+# Задание 11. Ввести число, определить отрицательно оно или положительно.
+# dig = (input('Введите число '))
+# if dig > '0':
+#     print('Число положительное')
+# if dig < '0':
+#     print('Число отрицательное')
